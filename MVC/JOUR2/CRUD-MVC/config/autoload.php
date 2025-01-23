8e16ef9e188d214e469f29f5075b9bae398d705f
Tandé Hugo <htande@hotmail.fr>
2025-01-23 12:59:29 +0000

    userManager

--- MVC/JOUR2/CRUD-MVC/config/autoload.php
+++ MVC/JOUR2/CRUD-MVC/config/autoload.php
@@ -3,3 +3,4 @@ require_once __DIR__ ."/Router.php";
 require_once __DIR__ ."/../controllers/UserController.php";
 require_once __DIR__ ."/../models/Users.php";
 require_once __DIR__ ."/../managers/AbstractManager.php";
+require_once __DIR__ ."/../managers/UserManager.php";
---
+++ MVC/JOUR2/CRUD-MVC/managers/UserManager.php
@@ -0,0 +1,63 @@
+<?php
+
+class UserManager extends AbstractManager
+{
+    public function __construct()
+    {
+        parent::__construct;
+    }
+    
+    // 
+    // METHOD
+    // 
+    
+    public function findAll() : array
+    {
+        $query = $this->db->query("SELECT * FROM users");
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+        
+        $users = [];
+        foreach ($usersData as $data) {
+            $users[] = new User($data['first_name'], $data['last_name'], $data['email'], $data['id']);
+        }
+        return $users;
+    }
+    
+    public function findOne(int $id) : array
+    {
+        $query = $this->db->prepare("SELECT * FROM users WHERE id=:id");
+        $query->execute(["id" => $id]);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+        
+        if ($data) {
+            return new User($data['firstname'], $data['lastname'], $data['email'], $data['id']);
+        }
+        return null;
+    }
+    
+    public function create(User $user): void
+    {
+        $query = $this->db->prepare("INSERT INTO users (first_name, last_name, email) VALUES (:first_name, :last_name, :email)");
+        $query->execute([
+            "first_name" => $user->getFirstName(),
+            "last_name" => $user->getLastName(),
+            "email" => $user->getEmail()
+        ]);
+        $user->setId($this->db->lastInsertId());
+    }
+    public function update(User $user): void
+    {
+        $query = $this->db->prepare("UPDATE users SET first_name = :first_name, last_name = :last_name, email = :email WHERE id = :id");
+        $query->execute([
+            "id" => $user->getId(),
+            "first_name" => $user->getFirstName(),
+            "last_name" => $user->getLastName(),
+            "email" => $user->getEmail()
+        ]);
+    }
+    public function delete(User $user): void
+    {
+        $query = $this->db->prepare("DELETE FROM users WHERE id = :id");
+        $query->execute(["id" => $id]);
+    }
+}
