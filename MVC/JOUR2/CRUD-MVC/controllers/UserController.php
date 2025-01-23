8e56e088799d9207a1e94330e9d4d9e9fb912f40
Tandé Hugo <htande@hotmail.fr>
2025-01-23 13:15:31 +0000

    test

--- MVC/JOUR2/CRUD-MVC/controllers/UserController.php
+++ MVC/JOUR2/CRUD-MVC/controllers/UserController.php
@@ -6,21 +6,30 @@ class UserController
     {
         $route = "show_user";
         require_once __DIR__. "/../templates/layout.phtml";
+        // $userManager = newUserManager();
+        // $userManager->findOne();
     }
     public function create(): void
     {
         $route = "create_user";
         require_once __DIR__. "/../templates/layout.phtml";
+        // $userManager = newUserManager();
+        // $userManager->create();
     }
     public function update(): void
     {
         $route = "update_user";
         require_once __DIR__. "/../templates/layout.phtml";
+        // $userManager = newUserManager();
+        // $userManager->update();
     }
     public function list(): void
     {
         $route = "list";
         require_once __DIR__. "/../templates/layout.phtml";
+        
+        // $userManager = newUserManager();
+        // $userManager->findAll();
     }
     
 }
