76fc2af7ed4b4a7c3f4a3c9e1bf824aa0410c5a8
Tandé Hugo <htande@hotmail.fr>
2025-01-23 09:26:16 +0000

    Test templates et UserController

--- MVC/JOUR2/CRUD-MVC/config/autoload.php
+++ MVC/JOUR2/CRUD-MVC/config/autoload.php
@@ -1,2 +1,3 @@
 <?php 
 require_once __DIR__ ."/Router.php";
+require_once __DIR__ ."/../controllers/UserController.php";
--- MVC/JOUR2/CRUD-MVC/controllers/UserController.php
+++ MVC/JOUR2/CRUD-MVC/controllers/UserController.php
@@ -5,21 +5,22 @@ class UserController
     public function show(): void
     {
         $route = "show_user";
-        require_once __DIR__. "/../templates/users/layout.phtml";
+        require_once __DIR__. "/../templates/layout.phtml";
     }
     public function create(): void
     {
         $route = "create_user";
-        require_once __DIR__. "/../templates/users/layout.phtml";
+        require_once __DIR__. "/../templates/layout.phtml";
     }
     public function update(): void
     {
         $route = "update_user";
-        require_once __DIR__. "/../templates/users/layout.phtml";
+        require_once __DIR__. "/../templates/layout.phtml";
     }
     public function list(): void
     {
-        $route = "";
-        require_once __DIR__. "/../templates/users/layout.phtml";
+        
+        require_once __DIR__. "/../templates/layout.phtml";
     }
+    
 }
